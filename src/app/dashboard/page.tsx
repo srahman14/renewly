4d0a1c901ab6cb4016a0d7268e1af0eab3754b60
srahman14 <s.rahman14@hotmail.com>
2026-07-15 19:31:46 +0100

    feat: inital project setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <div>
+        <p className="text-4xl tracking-tight font-medium">Dashboard page</p>
+      </div>
+    </main>
+  )
+}
+
+export default DashboardPage
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <div>
+        <p className="text-4xl tracking-tight font-medium">Home page</p>
+      </div>
+    </main>
+  );
+}
